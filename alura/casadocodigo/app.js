e7ee4c7d8b38cace5def3ac1e3e3613f82f62eff
Joseph <joseluis.bsa@gmail.com>
2018-05-26 20:31:44 -0300

    separando o codigo

--- alura/casadocodigo/app.js
+++ alura/casadocodigo/app.js
@@ -1,13 +1,6 @@
-let express = require('express')
-let app = express()
-
-app.set('view engine', 'ejs')
-
-app.get('/produtos', function(req, res){
-	res.render("produtos/lista")
-})
+let app = require('./config/express')()
+let rotasProdutos = require('./app/routes/produtos')(app)
 
 app.listen(3000, function(){
 	console.log("server ok")
 })
-
