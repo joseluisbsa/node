2eced0a2180030ade7a97d1109f9d52e1c8f9a45
Joseph <joseluis.bsa@gmail.com>
2018-06-03 15:23:21 -0300

    testando a app com mocha

---
+++ alura/casadocodigo/test/produtos.js
@@ -0,0 +1,21 @@
+let http = require('http')
+let assert = require('assert')
+
+describe('#ProdutosController', function () {
+    it('#listagem json', function (done) {
+        let config = {
+            hostname: 'localhost',
+            port: 3000,
+            path: '/produtos',
+            headers: {
+                'Accept': 'application/json'
+            }
+        }
+
+        http.get(config, function (res) {
+            assert.equal(res.statusCode, 200)
+            assert.equal(res.headers['content-type'], 'application/json; charset=utf-8')
+            done()
+        })
+    })
+})
